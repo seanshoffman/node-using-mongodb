cea1c2c6b0d2744d2fac52e1ee96a05062672b57
Sean Hoffman <sean-hoffman@pluralsight.com>
2020-04-28 21:00:33 -0600

    m3t2

---
+++ src/models/models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Enter a product'
+    },
+    description: {
+        type: String,
+        required: 'Enter a description'
+    },
+    category: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
