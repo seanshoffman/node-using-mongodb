9ad12cf3b051d2133c8096e7d3f8ab1200cff00d
Sean Hoffman <sean-hoffman@pluralsight.com>
2020-04-30 12:48:21 -0600

    m4t1t2t3

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -10,5 +10,23 @@ export const addnewProduct = (req, res) => {
             res.send(err);
         }
         res.json(Product);
-    })
+    });
+}
+
+export const getProducts = (req, res) => {
+    Product.find({}, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+}
+
+export const getProductWithID = (req, res) => {
+    Product.findById(req.params.ProductId, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
-        .post(addnewProduct);
+        .post(addnewProduct)
+        .get(getProducts);
 }
 
 export default routes;
